05545fdd2bfc0bdde63f3067ccbf8ae2fe77a861
Kera Cudmore <kera.cudmore@hotmail.com>
2022-10-15 22:38:08 +0000

    set up allauth

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -59,13 +65,33 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request',  # required by Allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'  # account authentication using either username or email 
+ACCOUNT_EMAIL_REQUIRED = True  # email required to register for the site
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'  # email verified so we know its a real email
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True  # email has to be entered twice on registration page to ensure no typos
+ACCOUNT_USERNAME_MIN_LENGTH = 4  # min length for the username
+LOGIN_URL = '/accounts/login'  # login url
+LOGIN_REDIRECT_URL = '/'  # this is the url to redirect to after logging in
 
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
