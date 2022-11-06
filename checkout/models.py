420cd4117e99ad1bb9c8088accd2f1fd1ef31486
Kera Cudmore <kera.cudmore@hotmail.com>
2022-11-06 21:54:25 +0000

    created checkout app and created models

---
+++ checkout/models.py
@@ -0,0 +1,37 @@
+import uuid
+
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from django.db import models
+
+# Create your models here.
+
+
+class Order(models.Model):
+    # order no is going to be automatically generated, we want it to be unique & permanent so users can find previous orders 
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=False)
+    town_or_city = models.CharField(max_length=40, null=True, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    # auto_now_add automatically sets the date and time when the order is created
+    date = models.DateTimeField(auto_now_add=True)
+    # these last 3 fields are calculated using a model method each time an order is saved
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
