bedf7473c08578bfc510c57f84a9035a8684bc32
Kera Cudmore <kera.cudmore@hotmail.com>
2022-11-19 22:59:29 +0000

    add: webhook handler

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+
+
+class StripeWH_Handler:
+    """
+    Handles Stripe Webhooks
+    """
+
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        """
+        Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200
+        )
