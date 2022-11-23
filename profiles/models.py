27a9a457ec15b40f405b93125c2101ed6e4de56c
Kera Cudmore <kera.cudmore@hotmail.com>
2022-11-23 18:46:23 +0000

    add: profile form views & updated template

--- profiles/models.py
+++ profiles/models.py
@@ -19,21 +19,6 @@ class UserProfile(models.Model):
         null=True,
         blank=True
         )
-    default_country = CountryField(
-        blank_label='Country *',
-        null=True,
-        blank=True
-        )
-    default_postcode = models.CharField(
-        max_length=20,
-        null=True,
-        blank=True
-        )
-    default_town_or_city = models.CharField(
-        max_length=40,
-        null=True,
-        blank=True
-        )
     default_street_address1 = models.CharField(
         max_length=80,
         null=True,
@@ -44,8 +29,23 @@ class UserProfile(models.Model):
         null=True,
         blank=True
         )
+    default_town_or_city = models.CharField(
+        max_length=40,
+        null=True,
+        blank=True
+    )
     default_county = models.CharField(
         max_length=80,
+        null=True,
+        blank=True
+        )
+    default_postcode = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True
+        )
+    default_country = CountryField(
+        blank_label='Country',
         null=True,
         blank=True
         )
