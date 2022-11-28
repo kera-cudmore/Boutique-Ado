3b09b874468ddaa1d46a73c592aa14aee15a3f50
Kera Cudmore <kera.cudmore@hotmail.com>
2022-11-28 21:23:59 +0000

    add: cache control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -194,6 +194,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # USE AWS S3 Bucket if USE_AWS in os.environ
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'kera-cudmore-boutique-ado'
     AWS_S3_REGION_NAME = 'eu-west-2'
@@ -210,6 +216,7 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
 
 # for delivery calculations
 FREE_DELIVERY_THRESHOLD = 50
