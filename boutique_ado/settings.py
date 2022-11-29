1f3e94b48f81d7f72d02ad83f8b6dad86c6a0637
Kera Cudmore <kera.cudmore@hotmail.com>
2022-11-29 00:09:16 +0000

    fix: stripe wh variable to match env variable in heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -227,7 +227,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-STRIPE_WH_KEY = os.getenv('STRIPE_WH_SECRET', 'python')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', 'python')
 DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 
 # Default primary key field type
