021da13d4f92bdc5589ed8254e9521ec097d39c2
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-23 11:06:14 +0000

    fix: validation missing ;

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -120,5 +120,5 @@ form.addEventListener('submit', function(ev) {
     }).fail(function () {
         // Just reload the page, the error will be in django messages
         location.reload();
-    })
+    });
 });
