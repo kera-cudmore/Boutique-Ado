5876de28470af3cfbdc5b762d83e66fe16e40f64
Kera Cudmore <kera.cudmore@hotmail.com>
2023-07-06 18:56:16 +0100

    add: 127.0.0.1 to allowed hosts

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['kera-cudmore-boutique-ado.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['kera-cudmore-boutique-ado.herokuapp.com', 'localhost', "127.0.0.1"]
 
 
 # Application definition
